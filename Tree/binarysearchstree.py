9a5be2a703eb919f5d3b2e812b4af1c6f47a671a
hari <hari.prasad@gingerwebs.in>
2021-02-28 12:14:47 +0530

    BST

---
+++ Tree/binarysearchstree.py
@@ -0,0 +1,46 @@
+class BinarySearchtreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def add_child(self, data):
+        if data == self.data:
+            return 
+        if data < self.data:
+            # add data in left sub tree
+            if(self.left):
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchtreeNode(data)
+        else:
+            # add data in right sub tree
+            if(self.right):
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchtreeNode(data)
+
+    def in_order_traversal(self):
+        elements = []
+        #LPR traversal
+        #visit left tree
+        if(self.left):
+            elements += self.left.in_order_traversal()
+        #visit base tree
+        elements.append(self.data);
+        #vsiit right tree
+        if(self.right):
+            elements += self.right.in_order_traversal()
+        return elements;
+
+def build_tree(elements):
+    root = BinarySearchtreeNode(elements[0])
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+    
+    return root
+
+if __name__ == '__main__':
+    numbers = [17,4,1,20,9,23,18,34]
+    numbers_tree = build_tree(numbers);
+    print(numbers_tree.in_order_traversal())
