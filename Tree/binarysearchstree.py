ecc722ec9d0fa98a9fbee49575ff9721a6397e4c
hari <hari.prasad@gingerwebs.in>
2021-02-28 12:32:56 +0530

    Binary Search Tree

--- Tree/binarysearchstree.py
+++ Tree/binarysearchstree.py
@@ -32,6 +32,21 @@ class BinarySearchtreeNode:
         if(self.right):
             elements += self.right.in_order_traversal()
         return elements;
+    
+    def search(self, val):
+        if self.data == val:
+            return True
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False;
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
 
 def build_tree(elements):
     root = BinarySearchtreeNode(elements[0])
@@ -44,3 +59,5 @@ if __name__ == '__main__':
     numbers = [17,4,1,20,9,23,18,34]
     numbers_tree = build_tree(numbers);
     print(numbers_tree.in_order_traversal())
+    print(numbers_tree.search(20))
+    print(numbers_tree.search(49))
